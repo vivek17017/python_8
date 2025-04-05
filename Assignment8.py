8cd2801e30706352cfbca9181bffdceb0ad5d4e0
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-05 22:35:15 +0530

    Add files via upload

---
+++ Assignment8.py
@@ -0,0 +1,26 @@
+
+x=input("Enter your name ")
+print(x)
+
+
+#Q2
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+c=a+b
+print(c)
+
+#Q3
+a=int(input("Enter radius "))
+area=3.14*a*a
+print(area)
+
+
+#Q4
+a=int(input("Enter a number "))
+print(a*a)
+
+
+#Q5
+a=input("Enter a character ")
+print(ord(a))
+
